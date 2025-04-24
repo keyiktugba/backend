b33eab8d529406f600d13b35bd955f839913627f
keyiktugba <114024460+keyiktugba@users.noreply.github.com>
2025-04-24 17:16:30 +0300

    .

--- routes/moveRoutes.js
+++ routes/moveRoutes.js
@@ -1,8 +1,11 @@
+//moveRoutes.js
 const express = require('express');
 const router = express.Router();
-const {createMove} = require('../controllers/moveController');
+const {
+  createMove
+} = require('../controllers/moveController');
 
-// Hamle işlemleri
+// Oyuna katılma ya da yeni oyun oluşturma
 router.post('/create-Move', createMove);
 
 module.exports = router;
