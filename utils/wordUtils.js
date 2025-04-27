54009bfbf162877dfc5b1902830a845615767d3d
caglagok <114026286+caglagok@users.noreply.github.com>
2025-04-27 22:41:21 +0300

    bonustile

--- utils/wordUtils.js
+++ utils/wordUtils.js
@@ -16,27 +16,36 @@ const letterPoints = {
 
 // 3. Bonus taşları
 const bonusTiles = {
-  K3: [{ row: 0, col: 0 }, { row: 0, col: 14 }, { row: 14, col: 0 }, { row: 14, col: 14 }],
+  K3: [
+    { row: 0, col: 2 }, { row: 0, col: 12 },
+    { row: 2, col: 0 }, { row: 2, col: 14 },
+    { row: 12, col: 0 }, { row: 12, col: 14 }, 
+    { row: 14, col: 2 }, { row: 14, col: 12 },
+  ],
   H3: [
-    { row: 1, col: 5 }, { row: 1, col: 9 }, { row: 5, col: 1 }, { row: 5, col: 13 },
-    { row: 9, col: 1 }, { row: 9, col: 13 }, { row: 13, col: 5 }, { row: 13, col: 9 }
+    { row: 1, col: 1 }, { row: 1, col: 13 }, 
+    { row: 4, col: 4 }, { row: 4, col: 10 }, 
+    { row: 10, col: 4 }, { row: 10, col: 10 }, 
+    { row: 13, col: 1 }, { row: 13, col: 13 },
   ],
   K2: [
-    { row: 1, col: 1 }, { row: 2, col: 2 }, { row: 3, col: 3 }, { row: 4, col: 4 },
-    { row: 10, col: 10 }, { row: 11, col: 11 }, { row: 12, col: 12 }, { row: 13, col: 13 },
-    { row: 1, col: 13 }, { row: 2, col: 12 }, { row: 3, col: 11 }, { row: 4, col: 10 },
-    { row: 10, col: 4 }, { row: 11, col: 3 }, { row: 12, col: 2 }, { row: 13, col: 1 }
+    { row: 3, col: 3 }, { row: 3, col: 11 },
+    { row: 7, col: 2 }, { row: 7, col: 12 }, 
+    { row: 11, col: 3 }, { row: 11, col: 11 }, 
+    { row: 12, col: 7 }, { row: 2, col: 7 },
   ],
   H2: [
-    { row: 0, col: 3 }, { row: 0, col: 11 }, { row: 2, col: 6 }, { row: 2, col: 8 },
-    { row: 3, col: 0 }, { row: 3, col: 7 }, { row: 3, col: 14 }, { row: 6, col: 2 },
-    { row: 6, col: 6 }, { row: 6, col: 8 }, { row: 6, col: 12 }, { row: 7, col: 3 },
-    { row: 7, col: 11 }, { row: 8, col: 2 }, { row: 8, col: 6 }, { row: 8, col: 8 },
-    { row: 8, col: 12 }, { row: 11, col: 0 }, { row: 11, col: 7 }, { row: 11, col: 14 },
-    { row: 12, col: 6 }, { row: 12, col: 8 }, { row: 14, col: 3 }, { row: 14, col: 11 }
+    { row: 0, col: 5 }, { row: 0, col: 9 },
+    { row: 1, col: 6 }, { row: 1, col: 8 },
+    { row: 5, col: 0 }, { row: 5, col: 5 }, { row: 5, col: 9 }, { row: 5, col: 14 },
+    { row: 6, col: 1 }, { row: 6, col: 6 }, { row: 6, col: 8 }, { row: 6, col: 13 },
+    { row: 8, col: 1 }, { row: 8, col: 6 }, { row: 8, col: 8 }, { row: 8, col: 13 },
+    { row: 9, col: 0 }, { row: 9, col: 5 }, { row: 9, col: 9 }, { row: 9, col: 14 },
+    { row: 13, col: 5 }, { row: 13, col: 9 },
+    { row: 14, col: 6 }, { row: 14, col: 8 },
   ],
   CENTER: [{ row: 7, col: 7 }]
-};
+};  
 
 // 4. Sözlük kontrolü
 //    Verilen kelimeyi (string) sözlükte arar. Büyük/küçük harf duyarsız.
