fc68a5869c3999e9932ff1d72ef92ca33aecf99d
caglagok <114026286+caglagok@users.noreply.github.com>
2025-05-04 16:53:14 +0300

    socket

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-//index.js
+// index.js
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 const cors = require('cors');
 const http = require('http');
-const socketIo = require('socket.io');
+const setupSocket = require('./socket');
 const apiRoutes = require('./api');
 
 dotenv.config();
@@ -24,54 +24,8 @@ app.get('/', (req, res) => {
 });
 
 const server = http.createServer(app);
-const io = socketIo(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-global.io = io;
-io.on('connection', (socket) => {
-  console.log('Yeni bir kullanıcı bağlandı', socket.id);
-  
-  socket.on('join-game', (gameId) => {
-    socket.join(gameId);
-    console.log(`Kullanıcı ${socket.id} ${gameId} odasına katıldı`);
-  });
-  
-  socket.on('leave-game', (gameId) => {
-    socket.leave(gameId);
-    console.log(`Kullanıcı ${socket.id} ${gameId} odasından ayrıldı`);
-  });
-  
-  socket.on('game-move', (data) => {
-    io.to(data.gameId).emit('move-made', {
-      playerId: data.playerId,
-      move: data.move
-    });
-  });
-  
-  socket.on('word-found', (data) => {
-    io.to(data.gameId).emit('word-found', {
-      playerId: data.playerId,
-      word: data.word,
-      points: data.points
-    });
-  });
-  
-  socket.on('game-over', (data) => {
-    io.to(data.gameId).emit('game-over', {
-      winnerId: data.winnerId,
-      scores: data.scores
-    });
-  });
-  
-  socket.on('disconnect', () => {
-    console.log('Bir kullanıcı ayrıldı', socket.id);
-  });
-});
+const io = setupSocket(server); 
 
 server.listen(PORT, () => {
   console.log(`Sunucu çalışıyor 🚀`);
 });
-module.exports = { io };
