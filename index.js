b77a5b0552d4cb60d6c8fe8b4b35aa79be218396
caglagok <114026286+caglagok@users.noreply.github.com>
2025-05-04 17:24:39 +0300

    socket deleted

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-// index.js
+//index.js
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
 const cors = require('cors');
 const http = require('http');
-const setupSocket = require('./socket');
+const socketIo = require('socket.io');
 const apiRoutes = require('./api');
 
 dotenv.config();
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -22,10 +25,53 @@ mongoose.connect("mongodb+srv://medihatugbakeyik:Hh5U8sFS421LnavH@wordmine1.klza
 app.get('/', (req, res) => {
   res.send('Kelime Mayınları API çalışıyor');
 });
-
 const server = http.createServer(app);
-const io = setupSocket(server); 
-global.io = io;
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+io.on('connection', (socket) => {
+  console.log('Yeni bir kullanıcı bağlandı', socket.id);
+  
+  socket.on('join-game', (gameId) => {
+    socket.join(gameId);
+    console.log(`Kullanıcı ${socket.id} ${gameId} odasına katıldı`);
+  });
+  
+  socket.on('leave-game', (gameId) => {
+    socket.leave(gameId);
+    console.log(`Kullanıcı ${socket.id} ${gameId} odasından ayrıldı`);
+  });
+  
+  socket.on('game-move', (data) => {
+    io.to(data.gameId).emit('move-made', {
+      playerId: data.playerId,
+      move: data.move
+    });
+  });
+  
+  socket.on('word-found', (data) => {
+    io.to(data.gameId).emit('word-found', {
+      playerId: data.playerId,
+      word: data.word,
+      points: data.points
+    });
+  });
+  
+  socket.on('game-over', (data) => {
+    io.to(data.gameId).emit('game-over', {
+      winnerId: data.winnerId,
+      scores: data.scores
+    });
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('Bir kullanıcı ayrıldı', socket.id);
+  });
+});
+
 server.listen(PORT, () => {
   console.log(`Sunucu çalışıyor 🚀`);
 });
